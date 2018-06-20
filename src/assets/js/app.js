2aa92ede3ed31605f57c9f3d09e294923d0b9113
antonbuyuk <anton.buyuk@abiatec.com>
2018-06-20 12:53:33 +0300

    done

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     	$(this).toggleClass('lukas');
     	$(this).children('.i-like').toggleClass('i-like-fill');
     });
-
+	
     $('.owl-carousel').owlCarousel({
 	    loop:false,
         margin:30,
@@ -13,7 +13,9 @@ $(document).ready(function() {
         navText: ['<i class="i-back"></i>','<i class="i-next"></i>'],
 	    responsive:{
 	        0:{
-	            items:1
+				items:1,
+				dots: false,
+				nav: false
 	        },
 	        640:{
 	            items:2
@@ -22,7 +24,34 @@ $(document).ready(function() {
 	            items:4
 	        }
 	    }
-	})
+	});
+
+	$('.carousel-caralog').owlCarousel({
+	    loop:false,
+        margin:30,
+        nav:true,
+        navText: ['<i class="i-back"></i>','<i class="i-next"></i>'],
+	    responsive:{
+	        0:{
+				items:1,
+				dots: false,
+				nav: false
+	        },
+	        640:{
+	            items:2
+	        },
+	        1000:{
+	            items:4
+	        }
+	    }
+	});
+
+    $('.file_remove').click(function(event) {
+    	$(this).next('.file_img').attr('src', '');
+    	$(this).parents('.file_upload').removeClass('is-active');
+	});
+	
+	$('a[href^="#"]').click(function(){var t=$(this).attr("href");return $("html, body").animate({scrollTop:$(t).offset().top},1250),!1})
 })
 
 function readURL(input) {
@@ -37,9 +66,48 @@ function readURL(input) {
         
         reader.readAsDataURL(input.files[0]);
     }
+}
 
-    $('.file_upload.is-active').click(function() {
-    	$(this).find('.file_img').attr('src', '');
-    	$(this).removeClass('is-active');
-    });
+function readURL2(input) {
+
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('.file_img-2').attr('src', e.target.result);
+            $('.file_img-2').parents('.file_upload').addClass('is-active');
+        };
+        
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+
+function readURL3(input) {
+
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('.file_img-3').attr('src', e.target.result);
+            $('.file_img-3').parents('.file_upload').addClass('is-active');
+        };
+        
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+
+function readURL4(input) {
+
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('.file_img-4').attr('src', e.target.result);
+            $('.file_img-4').parents('.file_upload').addClass('is-active');
+        };
+        
+        reader.readAsDataURL(input.files[0]);
+    }
 }
